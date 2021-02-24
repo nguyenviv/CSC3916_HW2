be803aa22633157385d1bdbbfae070a3ae7374e8
nguyenviv <72362744+nguyenviv@users.noreply.github.com>
2021-02-23 19:55:56 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ router.route('/movies')
     .delete(authController.isAuthenticated, function(req, res) {
         console.log(req.body);
         res = res.status(200);
+        res.json({msg: 'movie deleted'})
         if (req.get('Content-Type')) {
             res = res.type(req.get('Content-Type'));
         }
